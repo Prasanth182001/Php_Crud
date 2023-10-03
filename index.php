cc6dda21c6eee94c5ccf76323ac16f020c963c15
Prasanth <124224976+Prasanth182001@users.noreply.github.com>
2023-10-03 13:39:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                 <td><a href="delete.php?id=<?php echo $id?>">Delete</a></td>
             </tr>
             <?php
-            $id++
+            $id++ ;
         }
         
         ?>
